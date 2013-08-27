3196ba1401b83096da38efc2f81666e696907e40
rl <rl@396f6da5-dee2-485a-8d28-aac8f88cb0cd>
2013-08-27 12:30:46 +0000

    - Added translation.
    git-svn-id: http://svn.grenlandweb.no/svn/commercial/bratsberggruppen/trunk/extension/ezselection2@7487 396f6da5-dee2-485a-8d28-aac8f88cb0cd

---
+++ translations/nor-NO/translation.ts
@@ -0,0 +1,92 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezselection2/class/edit</name>
+    <message>
+        <source>Option list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New option</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Multiple choice</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Use Checkboxes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delimiter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Useful selection options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Quickly add selection options. Use a comma to seperate entries</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezselection2/class/view</name>
+    <message>
+        <source>Option list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Identifier</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Value</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Multiple choice</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Checkbox style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delimiter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Database query</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezselection2/datatypes</name>
+    <message>
+        <source>Selection 2</source>
+        <comment>Datatype name</comment>
+        <translation>Selection 2</translation>
+    </message>
+    <message>
+        <source>This is a required field.</source>
+        <translation>Dette feltet er påkrevd</translation>
+    </message>
+    <message>
+        <source>No POST variable. Please check your configuration.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
